7980506a6540306dcf0e0ce4d1fc7e4d041492d7
Khalid Ramadan <khalid.waleed.ra@gmail.com>
2024-11-19 09:23:27 +0200

    fixing code

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,31 +4,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character 
+public class Character
 {
    private string name;
    private int health;
 
-   public Character(string name, int health){
+   public Character(string name, int health)
+   {
       this.name = name;
       this.health = health;
    }
-   public string Name{
-    set{
-       name = value;
-    }
-    get{
-       return name;
-    }
+   public string Name
+   {
+      set
+      {
+         name = value;
+      }
+      get
+      {
+         return name;
+      }
    }
-   public int Health{
-    set{
-      if (health <= 100 && health > 0)
-       health = value;
-      else Debug.Log("Invalid Input Value");
-    }
-    get{
-       return health;
-    }
+   public int Health
+   {
+      set
+      {
+         if (health > 100)
+            health = 100;
+         else if (health < 0)
+            health = 0;
+         else
+            health = value;
+      }
+      get
+      {
+         return health;
+      }
    }
 }
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Player : Character
 {
-    public Player(string name, int health) : base(name, health){}
+  public Player(string name, int health) : base(name, health) { }
 
-    public void Heal(int value){
-      if(value > 0) this.Health += value;
-      else Debug.Log("Invalid Input Value");
-    }
+  public void Heal(int value)
+  {
+    if (value + Health > 100) this.Health = 100;
+
+    if (value > 0) this.Health += value;
+    else Debug.Log("Invalid Input Value");
+  }
 }
